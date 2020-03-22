3669a91fbded745299f12d776002764548d0b837
Oracien <amundbk@gmail.com>
2020-03-22 17:20:04 +0100

    Added libs

---
+++ lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Clock.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Err.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Err.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/FileInputStream.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/FileInputStream.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Lock.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/MemoryInputStream.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/MemoryInputStream.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Mutex.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Mutex.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Sleep.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/String.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/String.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Thread.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Thread.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/ThreadLocal.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/ThreadLocal.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Time.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Unix/ClockImpl.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Unix/ClockImpl.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Unix/MutexImpl.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Unix/MutexImpl.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Unix/SleepImpl.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Unix/SleepImpl.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Unix/ThreadImpl.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Unix/ThreadImpl.cpp.o"
+  "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/Unix/ThreadLocalImpl.cpp" "/home/amundbk/Documents/School/Graphics/Coursework/TDT4230-Assignment-1/lib/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Unix/ThreadLocalImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/SFML/include"
+  "lib/SFML/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
